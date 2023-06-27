4c01a74bbf3efa185b4176e63e531c9490d27215
IhorMarch <97352675+IhorMarch@users.noreply.github.com>
2023-06-27 22:49:45 +0300

    1st Task Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1) Render
+const galleryEl = document.querySelector(".gallery")
+const markup = galleryItems.map(({ preview, original, description })=>
+`<li class="gallery__item">
+        <a href="${original}" class="gallery__link">    
+          <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+         
+        /> 
+      </a>
+      </li>`)
+    .join("");
+ 
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+// 2) Modal
+let gallery = new SimpleLightbox(".gallery a");
+
+gallery.on('show.simplelightbox', function () {
+  gallery.options.captionsData = "alt"
+  gallery.options.captionDelay = 250
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import throttle from 'lodash.throttle';
+console.log(throttle);
+import Player from '@vimeo/player';
+console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
